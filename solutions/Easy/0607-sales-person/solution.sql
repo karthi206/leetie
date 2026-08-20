c3c69b0f908aa057297738798770a5f7a9e1218e
karthi206 <karthi206@users.noreply.github.com>
2026-08-20 04:17:22 +0000

    leetie: Add 607. Sales Person [Easy] (mysql)

---
+++ solutions/Easy/0607-sales-person/solution.sql
@@ -0,0 +1,20 @@
+-- ──────────────────────────────────────────────────
+-- Problem  : 607. Sales Person
+-- Difficulty: Easy
+-- Tags     : Database
+-- Link     : https://leetcode.com/problems/sales-person/
+-- Runtime  : 159 ms (beats 0%)
+-- Memory   : 0B (beats 0%)
+-- Language : mysql
+-- Copyright: (c) 2026 karthi206. All rights reserved.
+-- Synced by: leetie
+-- ──────────────────────────────────────────────────
+
+# Write your MySQL query statement below
+select name 
+from SalesPerson 
+where sales_id Not in (select o.sales_id 
+from Orders o
+join company c
+on o.com_id=c.com_id
+where c.name ='Red')
