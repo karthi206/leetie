93bc257040b99b2b17b1a81f38f057e721a14276
karthi206 <karthi206@users.noreply.github.com>
2026-08-20 05:10:25 +0000

    leetie: Add 627. Swap Sex of Employees [Easy] (mysql)

--- solutions/Easy/0627-swap-sex-of-employees/solution.sql
+++ solutions/Easy/0627-swap-sex-of-employees/solution.sql
@@ -3,7 +3,7 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/swap-sex-of-employees/
--- Runtime  : 299 ms (beats 25%)
+-- Runtime  : 306 ms (beats 21%)
 -- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
