1a7ca15c1c4c0aad5188ed8ac792c29e2b1950c8
karthi206 <karthi206@users.noreply.github.com>
2026-08-20 04:16:21 +0000

    leetie: Add 596. Classes With at Least 5 Students [Easy] (mysql)

--- solutions/Easy/0596-classes-with-at-least-5-students/solution.sql
+++ solutions/Easy/0596-classes-with-at-least-5-students/solution.sql
@@ -3,7 +3,7 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/classes-with-at-least-5-students/
--- Runtime  : 101 ms (beats 0%)
+-- Runtime  : 64 ms (beats 0%)
 -- Memory   : 0B (beats 0%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
