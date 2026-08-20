a1a783cd16f2ed94351ca25269ab5e6198000953
karthi206 <karthi206@users.noreply.github.com>
2026-08-20 04:15:26 +0000

    leetie: Add 584. Find Customer Referee [Easy] (mysql)

--- solutions/Easy/0584-find-customer-referee/solution.sql
+++ solutions/Easy/0584-find-customer-referee/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/find-customer-referee/
--- Runtime  : 62 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 509 ms (beats 61%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
