e8fa97b25ac61a268513064b81553cb79e9f415d
karthi206 <karthi206@users.noreply.github.com>
2026-08-20 04:07:13 +0000

    leetie: Add 577. Employee Bonus [Easy] (mysql)

--- solutions/Easy/0577-employee-bonus/solution.sql
+++ solutions/Easy/0577-employee-bonus/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/employee-bonus/
--- Runtime  : 98 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 1132 ms (beats 42%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
