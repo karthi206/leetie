5210c3ce1fb9ada005502167e21a1b5c6e234972
karthi206 <karthi206@users.noreply.github.com>
2026-08-26 13:28:38 +0000

    leetie: Add 214. Shortest Palindrome [Hard] (python3)

---
+++ solutions/Hard/0214-shortest-palindrome/solution.py
@@ -0,0 +1,33 @@
+# ──────────────────────────────────────────────────
+# Problem  : 214. Shortest Palindrome
+# Difficulty: Hard
+# Tags     : String, Rolling Hash, String Matching, Hash Function, Manacher, Z Algorithm, Knuth–Morris–Pratt Algorithm
+# Link     : https://leetcode.com/problems/shortest-palindrome/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 19256000 (beats 0%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        count = self.kmp(s[::-1], s)
+        return s[count:][::-1] + s
+    def kmp(self, txt: str, patt: str) -> int:
+        new_string = patt + '#' + txt
+        pi = [0] * len(new_string)
+        i = 1
+        k = 0
+        while i < len(new_string):
+            if new_string[i] == new_string[k]:
+                k += 1
+                pi[i] = k
+                i += 1
+            else:
+                if k > 0:
+                    k = pi[k - 1]
+                else:
+                    pi[i] = 0
+                    i += 1
+        return pi[-1]
