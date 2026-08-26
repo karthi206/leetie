270d309865ec69b0a6b0fbfd4bcbb04db9b6313c
karthi206 <karthi206@users.noreply.github.com>
2026-08-26 13:28:46 +0000

    leetie: Add 214. Shortest Palindrome [Hard] (python3)

--- solutions/Hard/0214-shortest-palindrome/solution.py
+++ solutions/Hard/0214-shortest-palindrome/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Hard
 # Tags     : String, Rolling Hash, String Matching, Hash Function, Manacher, Z Algorithm, Knuth–Morris–Pratt Algorithm
 # Link     : https://leetcode.com/problems/shortest-palindrome/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19256000 (beats 0%)
+# Runtime  : 47 ms (beats 73%)
+# Memory   : 23524000 (beats 29%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
