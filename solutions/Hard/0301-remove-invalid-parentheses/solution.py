72a47eba303f98d12231b0a5accd93fce0d97197
karthi206 <karthi206@users.noreply.github.com>
2026-08-28 03:47:36 +0000

    leetie: Add 301. Remove Invalid Parentheses [Hard] (python3)

---
+++ solutions/Hard/0301-remove-invalid-parentheses/solution.py
@@ -0,0 +1,52 @@
+# ──────────────────────────────────────────────────
+# Problem  : 301. Remove Invalid Parentheses
+# Difficulty: Hard
+# Tags     : String, Backtracking, Breadth-First Search
+# Link     : https://leetcode.com/problems/remove-invalid-parentheses/
+# Runtime  : 75 ms (beats 73%)
+# Memory   : 20048000 (beats 34%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+
+        def is_valid(s):
+            open= 0
+            for i in range(len(s)):
+                if s[i] == '(':
+                    open += 1
+                if s[i] == ')':
+                    open -= 1
+                if open < 0:
+                    return False
+            if open != 0:
+                return False
+            return True
+        
+        queue = deque([s])
+        visited = set()
+        visited.add(s)
+        result = []
+        found = False
+        
+        while queue:
+            for _ in range(len(queue)):
+                cur_s = queue.popleft()
+                if is_valid(cur_s):
+                    result.append(cur_s)
+                    found = True
+                if found:
+                    continue
+                
+                for i in range(len(cur_s)):
+                    if cur_s[i] =='(' or cur_s[i] == ')':
+                        new_s = cur_s[:i] + cur_s[i+1 :]
+                        if new_s not in visited:
+                            visited.add(new_s)
+                            queue.append(new_s)
+            if found:
+                return result
+        return [""]
