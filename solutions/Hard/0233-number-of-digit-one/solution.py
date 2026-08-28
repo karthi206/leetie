b595897fac141432c67064ad3ff9c4d5870561a6
karthi206 <karthi206@users.noreply.github.com>
2026-08-28 03:41:02 +0000

    leetie: Add 233. Number of Digit One [Hard] (python3)

--- solutions/Hard/0233-number-of-digit-one/solution.py
+++ solutions/Hard/0233-number-of-digit-one/solution.py
@@ -4,7 +4,7 @@
 # Tags     : Math, Dynamic Programming, Recursion
 # Link     : https://leetcode.com/problems/number-of-digit-one/
 # Runtime  : 0 ms (beats 0%)
-# Memory   : 19324000 (beats 0%)
+# Memory   : 19236000 (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
