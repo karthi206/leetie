674da8b020575a77dfaf814fa1e1c02893dfca2c
karthi206 <karthi206@users.noreply.github.com>
2026-08-28 16:44:30 +0000

    leetie: Add 804. Unique Morse Code Words [Easy] (python3)

--- solutions/Easy/0804-unique-morse-code-words/solution.py
+++ solutions/Easy/0804-unique-morse-code-words/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Array, Hash Table, String
 # Link     : https://leetcode.com/problems/unique-morse-code-words/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19436000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19148000 (beats 93%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
