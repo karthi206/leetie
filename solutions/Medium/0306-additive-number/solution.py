6167f6dca12fe35f90b834b4f68ea018f44d5f48
karthi206 <karthi206@users.noreply.github.com>
2026-08-28 16:41:52 +0000

    leetie: Add 306. Additive Number [Medium] (python3)

--- solutions/Medium/0306-additive-number/solution.py
+++ solutions/Medium/0306-additive-number/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : String, Backtracking
 # Link     : https://leetcode.com/problems/additive-number/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19456000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19396000 (beats 31%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
