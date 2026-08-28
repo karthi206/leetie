9a03620eef0035b77b36b43deeb17ee10a2d35c8
karthi206 <karthi206@users.noreply.github.com>
2026-08-28 16:54:05 +0000

    leetie: Add 821. Shortest Distance to a Character [Easy] (python3)

--- solutions/Easy/0821-shortest-distance-to-a-character/solution.py
+++ solutions/Easy/0821-shortest-distance-to-a-character/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Array, Two Pointers, String
 # Link     : https://leetcode.com/problems/shortest-distance-to-a-character/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19492000 (beats 0%)
+# Runtime  : 3 ms (beats 53%)
+# Memory   : 19336000 (beats 35%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
