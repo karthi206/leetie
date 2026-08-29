466a48db5830ec399ab7ce6d88edfa3514d96554
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:16:32 +0000

    leetie: Add 1050. Actors and Directors Who Cooperated At Least Three Times [Easy] (mysql)

--- solutions/Easy/1050-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
+++ solutions/Easy/1050-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
--- Runtime  : 91 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 373 ms (beats 77%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
