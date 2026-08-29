1db6712b965d9b91ab8c86d41d464759cd9122ed
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:18:36 +0000

    leetie: Add 1070. Product Sales Analysis III [Medium] (mysql)

--- solutions/Medium/1070-product-sales-analysis-iii/solution.sql
+++ solutions/Medium/1070-product-sales-analysis-iii/solution.sql
@@ -3,13 +3,14 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/product-sales-analysis-iii/
--- Runtime  : 68 ms (beats 0%)
+-- Runtime  : 88 ms (beats 0%)
 -- Memory   : 0B (beats 0%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
 -- ──────────────────────────────────────────────────
 
-select product_id, min(year) as first_year, quantity, price 
-from sales
-group by product_id
+# Write your MySQL query statement below
+select product_id,year as first_year, quantity, price
+from Sales
+where(product_id, year) in (select  product_id, min(year) from Sales group by product_id)
