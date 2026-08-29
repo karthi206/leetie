7613f0d3b239e0e7202eafc2eb8d7ef247631c10
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:31:20 +0000

    leetie: Add 319. Bulb Switcher [Medium] (python3)

--- solutions/Medium/0319-bulb-switcher/solution.py
+++ solutions/Medium/0319-bulb-switcher/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Math, Brainteaser
 # Link     : https://leetcode.com/problems/bulb-switcher/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19028000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19300000 (beats 53%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
