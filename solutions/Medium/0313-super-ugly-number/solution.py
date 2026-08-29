a926e234b712e14abc9224a03645cde47d81401f
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:27:53 +0000

    leetie: Add 313. Super Ugly Number [Medium] (python3)

--- solutions/Medium/0313-super-ugly-number/solution.py
+++ solutions/Medium/0313-super-ugly-number/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Math, Dynamic Programming
 # Link     : https://leetcode.com/problems/super-ugly-number/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19320000 (beats 0%)
+# Runtime  : 423 ms (beats 67%)
+# Memory   : 22876000 (beats 89%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
