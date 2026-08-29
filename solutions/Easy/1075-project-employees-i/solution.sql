9a45ab453c1761b3fb72f38110257bcb1bd5b5e3
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:19:20 +0000

    leetie: Add 1075. Project Employees I [Easy] (mysql)

--- solutions/Easy/1075-project-employees-i/solution.sql
+++ solutions/Easy/1075-project-employees-i/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/project-employees-i/
--- Runtime  : 103 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 496 ms (beats 92%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
