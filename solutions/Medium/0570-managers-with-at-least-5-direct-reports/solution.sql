652bbca648b3820f378ab32b14c859b7740011b0
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:10:45 +0000

    leetie: Add 570. Managers with at Least 5 Direct Reports [Medium] (mysql)

--- solutions/Medium/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ solutions/Medium/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
--- Runtime  : 65 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 331 ms (beats 94%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
