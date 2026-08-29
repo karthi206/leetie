760e60ba768f50260f6abe638189e4fcb0d334ce
karthi206 <karthi206@users.noreply.github.com>
2026-08-29 03:15:19 +0000

    leetie: Add 626. Exchange Seats [Medium] (mysql)

--- solutions/Medium/0626-exchange-seats/solution.sql
+++ solutions/Medium/0626-exchange-seats/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/exchange-seats/
--- Runtime  : 68 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 345 ms (beats 85%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
