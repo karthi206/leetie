15b978724fbf78a8ccba123b7362f20f8a663977
karthi206 <karthi206@users.noreply.github.com>
2026-08-31 14:46:11 +0000

    leetie: Add 1045. Customers Who Bought All Products [Medium] (mysql)

--- solutions/Medium/1045-customers-who-bought-all-products/solution.sql
+++ solutions/Medium/1045-customers-who-bought-all-products/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/customers-who-bought-all-products/
--- Runtime  : 115 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 601 ms (beats 65%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
