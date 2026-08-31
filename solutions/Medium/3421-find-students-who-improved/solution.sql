7c3848bfc9cccc66e34ad449331313a323b5f393
karthi206 <karthi206@users.noreply.github.com>
2026-08-31 14:46:52 +0000

    leetie: Add 3421. Find Students Who Improved [Medium] (mysql)

---
+++ solutions/Medium/3421-find-students-who-improved/solution.sql
@@ -0,0 +1,23 @@
+-- ──────────────────────────────────────────────────
+-- Problem  : 3421. Find Students Who Improved
+-- Difficulty: Medium
+-- Tags     : Database
+-- Link     : https://leetcode.com/problems/find-students-who-improved/
+-- Runtime  : 80 ms (beats 0%)
+-- Memory   : 0B (beats 0%)
+-- Language : mysql
+-- Copyright: (c) 2026 karthi206. All rights reserved.
+-- Synced by: leetie
+-- ──────────────────────────────────────────────────
+
+WITH Ranked AS (
+    SELECT
+    student_id,
+    subject,
+    FIRST_VALUE(score) OVER(PARTITION BY student_id,subject ORDER BY exam_date) AS first_score,
+    FIRST_VALUE(score) OVER(PARTITION BY student_id,subject ORDER BY exam_date DESC) AS latest_score
+    FROM Scores
+)
+SELECT DISTINCT * FROM Ranked
+WHERE first_score<latest_score
+ORDER BY student_id,subject
