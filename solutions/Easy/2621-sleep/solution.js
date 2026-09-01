98805fbf38b0801bab79720cb31614d32e378fd5
karthi206 <karthi206@users.noreply.github.com>
2026-09-01 08:35:13 +0000

    leetie: Add 2621. Sleep [Easy] (javascript)

--- solutions/Easy/2621-sleep/solution.js
+++ solutions/Easy/2621-sleep/solution.js
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/sleep/
-// Runtime  : 54 ms (beats 0%)
-// Memory   : 52152000 (beats 0%)
+// Runtime  : 37 ms (beats 0%)
+// Memory   : 53436000 (beats 0%)
 // Language : javascript
 // Copyright: (c) 2026 karthi206. All rights reserved.
 // Synced by: leetie
