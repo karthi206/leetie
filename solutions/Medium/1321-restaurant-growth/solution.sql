cacfc6a1ce73a4b77099a60119752d3b5459b978
karthi206 <karthi206@users.noreply.github.com>
2026-09-01 03:21:22 +0000

    leetie: Add 1321. Restaurant Growth [Medium] (mysql)

--- solutions/Medium/1321-restaurant-growth/solution.sql
+++ solutions/Medium/1321-restaurant-growth/solution.sql
@@ -3,13 +3,12 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/restaurant-growth/
--- Runtime  : 87 ms (beats 0%)
+-- Runtime  : 108 ms (beats 0%)
 -- Memory   : 0B (beats 0%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
 -- ──────────────────────────────────────────────────
-
 
 SELECT
     visited_on,
