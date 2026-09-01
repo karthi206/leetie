0ad14d8230342ab2ba90e975c1f33d66f179503c
karthi206 <karthi206@users.noreply.github.com>
2026-09-01 03:18:54 +0000

    leetie: Add 1251. Average Selling Price [Easy] (mysql)

--- solutions/Easy/1251-average-selling-price/solution.sql
+++ solutions/Easy/1251-average-selling-price/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Easy
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/average-selling-price/
--- Runtime  : 99 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 794 ms (beats 86%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
