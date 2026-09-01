0dc8e53850cd5b5eaed29fec51741e1b66be90b2
karthi206 <karthi206@users.noreply.github.com>
2026-09-01 08:36:37 +0000

    leetie: Add 2624. Snail Traversal [Medium] (javascript)

--- solutions/Medium/2624-snail-traversal/solution.js
+++ solutions/Medium/2624-snail-traversal/solution.js
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : N/A
 // Link     : https://leetcode.com/problems/snail-traversal/
-// Runtime  : 46 ms (beats 72%)
-// Memory   : 53644000 (beats 45%)
+// Runtime  : 47 ms (beats 0%)
+// Memory   : 53788000 (beats 0%)
 // Language : javascript
 // Copyright: (c) 2026 karthi206. All rights reserved.
 // Synced by: leetie
@@ -15,8 +15,15 @@
  * @param {number} colsCount
  * @return {Array<Array<number>>}
  */
-Array.prototype.snail = function(rowsCount, colsCount) {
-    
+Array.prototype.snail = function(numRows, numCols) {
+  if (numRows * numCols !== this.length) return [];
+  let result = Array(numRows).fill().map(() => []);
+  for (let row = 0; row < numCols; row++) {
+    for (let col = 0; col < numRows; col++) {
+      result[(row & 1) ? numRows - col - 1 : col].push(this[row * numRows + col]);
+    }
+  }
+  return result;
 }
 
 /**
