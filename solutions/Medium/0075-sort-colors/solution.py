f5f3294bc2bce89d9750c63d7da2e94889d335b0
karthi206 <karthi206@users.noreply.github.com>
2026-09-02 13:11:45 +0000

    leetie: Add 75. Sort Colors [Medium] (python3)

--- solutions/Medium/0075-sort-colors/solution.py
+++ solutions/Medium/0075-sort-colors/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Two Pointers, Sorting, Quicksort, Bubble Sort
 # Link     : https://leetcode.com/problems/sort-colors/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19248000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19240000 (beats 63%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
