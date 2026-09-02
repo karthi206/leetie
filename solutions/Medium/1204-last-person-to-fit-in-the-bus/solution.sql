1e271ef6bcf008b35864aae3302325aafb8b0839
karthi206 <karthi206@users.noreply.github.com>
2026-09-02 05:29:29 +0000

    leetie: Add 1204. Last Person to Fit in the Bus [Medium] (mysql)

--- solutions/Medium/1204-last-person-to-fit-in-the-bus/solution.sql
+++ solutions/Medium/1204-last-person-to-fit-in-the-bus/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/last-person-to-fit-in-the-bus/
--- Runtime  : 66 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 1736 ms (beats 17%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
