3c6867aaa4abcc5ad8037f6f613989b919463886
karthi206 <karthi206@users.noreply.github.com>
2026-09-02 05:29:07 +0000

    leetie: Add 1193. Monthly Transactions I [Medium] (mysql)

--- solutions/Medium/1193-monthly-transactions-i/solution.sql
+++ solutions/Medium/1193-monthly-transactions-i/solution.sql
@@ -3,8 +3,8 @@
 -- Difficulty: Medium
 -- Tags     : Database
 -- Link     : https://leetcode.com/problems/monthly-transactions-i/
--- Runtime  : 76 ms (beats 0%)
--- Memory   : 0B (beats 0%)
+-- Runtime  : 617 ms (beats 65%)
+-- Memory   : 0B (beats 100%)
 -- Language : mysql
 -- Copyright: (c) 2026 karthi206. All rights reserved.
 -- Synced by: leetie
