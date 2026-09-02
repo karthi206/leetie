3b7b8a506dd70da5cfc924ac9547fdd73f75714b
karthi206 <karthi206@users.noreply.github.com>
2026-09-02 14:06:09 +0000

    leetie: Add 3193. Count the Number of Inversions [Hard] (python3)

--- solutions/Hard/3193-count-the-number-of-inversions/solution.py
+++ solutions/Hard/3193-count-the-number-of-inversions/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Hard
 # Tags     : Array, Dynamic Programming
 # Link     : https://leetcode.com/problems/count-the-number-of-inversions/
-# Runtime  : 47 ms (beats 0%)
-# Memory   : 19200000 (beats 0%)
+# Runtime  : 46 ms (beats 0%)
+# Memory   : 19512000 (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
@@ -12,4 +12,26 @@
 
 class Solution:
     def numberOfPermutations(self, n: int, requirements: List[List[int]]) -> int:
-        return n-1
+        requirements = {end + 1: cnt for end, cnt in requirements}
+        
+        @cache
+        def dfs(distinctN: int, inversions: int) -> int:
+            """Return the number of permutations of distinctN distinct elements 
+            that satisfy the requirements and have exactly 'inversions' inversions.
+            """
+            if inversions < 0:
+                return 0
+            if distinctN in requirements and requirements[distinctN] != inversions:
+                return 0
+            if distinctN == 1:
+                return 1 if inversions == 0 else 0
+            
+            result = 0
+            for i in range(1, distinctN + 1):
+                inversionsCaused = distinctN - i
+                result += dfs(distinctN - 1, inversions - inversionsCaused)
+                
+            return result
+        
+        res = dfs(n, requirements[n])
+        return res % (10 ** 9 + 7)
