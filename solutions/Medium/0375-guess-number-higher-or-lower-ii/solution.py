cb0b1ae5cc70656a14319f7d693f7698d4d6edfe
karthi206 <karthi206@users.noreply.github.com>
2026-09-03 09:30:26 +0000

    leetie: Add 375. Guess Number Higher or Lower II [Medium] (python3)

--- solutions/Medium/0375-guess-number-higher-or-lower-ii/solution.py
+++ solutions/Medium/0375-guess-number-higher-or-lower-ii/solution.py
@@ -4,7 +4,7 @@
 # Tags     : Math, Dynamic Programming, Minimax, Game Theory
 # Link     : https://leetcode.com/problems/guess-number-higher-or-lower-ii/
 # Runtime  : 0 ms (beats 0%)
-# Memory   : 19312000 (beats 0%)
+# Memory   : 19268000 (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
@@ -12,23 +12,14 @@
 
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
-        if n == 1:
-            return 1
-        starting_index = 1 if n % 2 == 0 else 2
-        selected_nums = [i for i in range(starting_index, n, 2)]
-        selected_nums_length = len(selected_nums)
-        dp = [[0] * selected_nums_length for _ in range(selected_nums_length)]
-
-        for i in range(selected_nums_length):
-            dp[i][i] = selected_nums[i]
-
-        for length in range(2, selected_nums_length + 1):
-            for i in range(selected_nums_length - length + 1):
-                j = i + length - 1
-                dp[i][j] = float("inf")
-                for k in range(i, j + 1):
-                    dp_left = dp[i][k - 1] if k != 0 else 0
-                    dp_right = dp[k + 1][j] if k != j else 0
-                    dp[i][j] = min(dp[i][j], selected_nums[k] + max(dp_left, dp_right))
-
-        return dp[0][-1]
+        def dp(i, j, memo: dict):
+            if i >= j:
+                return 0
+            if (i, j) not in memo:
+                memo[(i, j)] = float('inf')
+                for x in range(i, j+1):
+                    lower = dp(i, x-1, memo)
+                    higher = dp(x+1, j, memo)
+                    memo[(i, j)] = min(memo[(i, j)], x + max(lower, higher))
+            return memo[(i, j)]
+        return dp(1, n, {})
