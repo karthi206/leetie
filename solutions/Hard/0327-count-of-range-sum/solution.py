222563997e93f54fd73faf0fb3629495e718da75
karthi206 <karthi206@users.noreply.github.com>
2026-09-03 03:00:16 +0000

    leetie: Add 327. Count of Range Sum [Hard] (python3)

--- solutions/Hard/0327-count-of-range-sum/solution.py
+++ solutions/Hard/0327-count-of-range-sum/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Hard
 # Tags     : Array, Binary Search, Divide and Conquer, Binary Indexed Tree, Segment Tree, Merge Sort, Ordered Set, Treap
 # Link     : https://leetcode.com/problems/count-of-range-sum/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19108000 (beats 0%)
+# Runtime  : 642 ms (beats 89%)
+# Memory   : 33028000 (beats 85%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
@@ -15,10 +15,8 @@ class Solution:
         psum = [0]
         for num in nums:
             psum.append(psum[-1] + num)
-
         result, slist = 0, SortedList()
         for pval in reversed(psum):
             result += slist.bisect_right(pval + upper) - slist.bisect_left(pval + lower)
             slist.add(pval)
-
         return result
