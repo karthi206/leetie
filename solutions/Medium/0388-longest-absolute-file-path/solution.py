3e2ccf091cfa174820aed035ee77f6bfeca97166
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 02:47:06 +0000

    leetie: Add 388. Longest Absolute File Path [Medium] (python3)

---
+++ solutions/Medium/0388-longest-absolute-file-path/solution.py
@@ -0,0 +1,30 @@
+# ──────────────────────────────────────────────────
+# Problem  : 388. Longest Absolute File Path
+# Difficulty: Medium
+# Tags     : String, Stack, Depth-First Search
+# Link     : https://leetcode.com/problems/longest-absolute-file-path/
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19300000 (beats 65%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        depth = 0
+        longest = 0
+        pathMap = { 0: 0 }
+
+        lines = input.split("\n")
+
+        for line in lines:
+            name = line.lstrip("\t")
+            depth = len(line) - len(name)
+
+            if "." in name:
+                longest = max(longest, pathMap[depth] + len(name))
+            else:
+                pathMap[depth + 1] = pathMap[depth] + len(name) + 1
+        
+        return longest
