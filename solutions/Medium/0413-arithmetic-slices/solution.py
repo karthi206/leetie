db0f0f2964c497b73225beb6bd2b0c57336f2c6f
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 12:49:51 +0000

    leetie: Add 413. Arithmetic Slices [Medium] (python3)

--- solutions/Medium/0413-arithmetic-slices/solution.py
+++ solutions/Medium/0413-arithmetic-slices/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Dynamic Programming, Sliding Window
 # Link     : https://leetcode.com/problems/arithmetic-slices/
-# Runtime  : 0 ms (beats 100%)
-# Memory   : 19508000 (beats 6%)
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 19328000 (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
