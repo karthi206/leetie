6a7b75c8d0c4f8d74b5ee1cb4f3e9d5d67f16258
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 12:50:25 +0000

    leetie: Add 416. Partition Equal Subset Sum [Medium] (python3)

--- solutions/Medium/0416-partition-equal-subset-sum/solution.py
+++ solutions/Medium/0416-partition-equal-subset-sum/solution.py
@@ -3,18 +3,28 @@
 # Difficulty: Medium
 # Tags     : Array, Dynamic Programming, Knapsack Problem, 0-1 Knapsack
 # Link     : https://leetcode.com/problems/partition-equal-subset-sum/
-# Runtime  : 0 ms (beats 100%)
-# Memory   : 19240000 (beats 86%)
+# Runtime  : 491 ms (beats 83%)
+# Memory   : 19424000 (beats 62%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
 # ──────────────────────────────────────────────────
 
 class Solution:
-    def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        le=len(A)
-        l=[0]*(le)
-        for i in range(2,le):
-            if A[i]-A[i-1] == A[i-1]-A[i-2]:
-                l[i]=1+l[i-1]
-        return sum(l)
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        
+        target = total // 2
+
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for n in nums:
+            for i in range(len(dp) - 1, n - 1, -1):
+                if dp[i]: continue
+                if dp[i-n]: dp[i] = True
+                if dp[-1]: return True
+        
+        return False
