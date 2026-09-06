efda15ab4b4b7d2b65d9a1dd5af9288515e7329c
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 12:36:27 +0000

    leetie: Add 400. Nth Digit [Medium] (python3)

--- solutions/Medium/0400-nth-digit/solution.py
+++ solutions/Medium/0400-nth-digit/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Math, Binary Search
 # Link     : https://leetcode.com/problems/nth-digit/
-# Runtime  : 0 ms (beats 100%)
-# Memory   : 19172000 (beats 87%)
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 19272000 (beats 0%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
