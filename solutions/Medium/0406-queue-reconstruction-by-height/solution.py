99387c8457868faecb9ad0ff82897ee9ad852d91
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 12:39:54 +0000

    leetie: Add 406. Queue Reconstruction by Height [Medium] (python3)

--- solutions/Medium/0406-queue-reconstruction-by-height/solution.py
+++ solutions/Medium/0406-queue-reconstruction-by-height/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Binary Indexed Tree, Segment Tree, Sorting
 # Link     : https://leetcode.com/problems/queue-reconstruction-by-height/
-# Runtime  : 2 ms (beats 86%)
-# Memory   : 19596000 (beats 94%)
+# Runtime  : 5 ms (beats 35%)
+# Memory   : 19684000 (beats 72%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
@@ -13,16 +13,8 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         output=[] 
-        
-        # sort the array in decreasing order of height 
-        # within the same height group, you would sort it in increasing order of k
-        # eg: Input : [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-        # after sorting: [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
         people.sort(key=lambda x: (-x[0], x[1]))                
         for a in people:
-            # Now let's start the greedy here
-            # We insert the entry in the output array based on the k value
-            # k will act as a position within the array
             output.insert(a[1], a)
         
         return output  
