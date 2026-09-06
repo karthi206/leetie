76c66ee88cea6e6e1a7eca03cefb5d1743d29233
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 14:40:10 +0000

    leetie: Add 50. Pow(x, n) [Medium] (python3)

---
+++ solutions/Medium/0050-powx-n/solution.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Problem  : 50. Pow(x, n)
+# Difficulty: Medium
+# Tags     : Math, Recursion
+# Link     : https://leetcode.com/problems/powx-n/
+# Runtime  : 2 ms (beats 18%)
+# Memory   : 19620000 (beats 21%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
