04b3aa8b9395257910fefeb356191e74ec589024
karthi206 <karthi206@users.noreply.github.com>
2026-09-06 12:49:49 +0000

    leetie: Add 413. Arithmetic Slices [Medium] (python3)

---
+++ solutions/Medium/0413-arithmetic-slices/solution.py
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────
+# Problem  : 413. Arithmetic Slices
+# Difficulty: Medium
+# Tags     : Array, Dynamic Programming, Sliding Window
+# Link     : https://leetcode.com/problems/arithmetic-slices/
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19508000 (beats 6%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        le=len(A)
+        l=[0]*(le)
+        for i in range(2,le):
+            if A[i]-A[i-1] == A[i-1]-A[i-2]:
+                l[i]=1+l[i-1]
+        return sum(l)
