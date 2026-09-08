c9cb64d45c87b1420a70065c01ccddcb261693df
karthi206 <karthi206@users.noreply.github.com>
2026-09-08 09:11:03 +0000

    leetie: Add 486. Predict the Winner [Medium] (python3)

--- solutions/Medium/0486-predict-the-winner/solution.py
+++ solutions/Medium/0486-predict-the-winner/solution.py
@@ -3,35 +3,22 @@
 # Difficulty: Medium
 # Tags     : Array, Math, Dynamic Programming, Recursion, Minimax, Game Theory, Zero-Sum Game
 # Link     : https://leetcode.com/problems/predict-the-winner/
-# Runtime  : 7129 ms (beats 5%)
-# Memory   : 19448000 (beats 36%)
+# Runtime  : 3 ms (beats 45%)
+# Memory   : 19812000 (beats 26%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
 # ──────────────────────────────────────────────────
 
-from decimal import *
+class Solution:
+    def predictTheWinner(self, A: List[int]) -> bool:
+        n = len(A)
+        if ~n & 1: return True
 
-class Solution(object):
-    def smallestGoodBase(self, n):
-        
-        getcontext().prec = 26
+        @cache
+        def maxDiff(i: int, j: int) -> int:
+            if i == j: return A[i]
+            return max(A[i] - maxDiff(i + 1, j),
+                       A[j] - maxDiff(i, j - 1))
 
-        for m in range(60,1,-1):
-            k=Decimal(2)
-            for i in range(300):
-                k = self.newtonMethod(k,m,int(n))
-        
-            basicRoundK = round(k)
-
-            if (round(k,6)==basicRoundK and basicRoundK>1):
-                return str(basicRoundK)
-
-    def newtonMethod(self,k,m,n):
-        numerator = (((k**m)-1)/(k-1))-n
-
-        denominatorPt1 = m*(k-1)*(k**(m-1))-((k**m)-1)
-        denominatorPt2 = (k-1)**2
-        totalDenom = denominatorPt1/denominatorPt2
-
-        return k - (numerator/totalDenom)
+        return maxDiff(0, n - 1) >= 0
