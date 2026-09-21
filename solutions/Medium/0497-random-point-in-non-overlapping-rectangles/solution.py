99ec5fa0b439185b82c9f232345d999a8622c2ed
karthi206 <karthi206@users.noreply.github.com>
2026-09-21 04:57:06 +0000

    leetie: Add 497. Random Point in Non-overlapping Rectangles [Medium] (python3)

---
+++ solutions/Medium/0497-random-point-in-non-overlapping-rectangles/solution.py
@@ -0,0 +1,19 @@
+# ──────────────────────────────────────────────────
+# Problem  : 497. Random Point in Non-overlapping Rectangles
+# Difficulty: Medium
+# Tags     : Array, Math, Binary Search, Reservoir Sampling, Prefix Sum, Ordered Set, Randomized
+# Link     : https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/
+# Runtime  : 31 ms (beats 57%)
+# Memory   : 23028000 (beats 22%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def __init__(self, r: List[List[int]]):
+        self.r,self.p = r,[*accumulate((x-a+1)*(y-b+1) for a,b,x,y in r)]
+
+    def pick(self) -> List[int]:
+        a,b,x,y = self.r[bisect_left(self.p,randint(1,self.p[-1]))]
+        return randint(a,x),randint(b,y)
