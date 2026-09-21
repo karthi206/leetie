0ab43b5bd0d0c4a8ebba87d99ab10b14e5d0aa91
karthi206 <karthi206@users.noreply.github.com>
2026-09-21 04:59:03 +0000

    leetie: Add 517. Super Washing Machines [Hard] (python3)

---
+++ solutions/Hard/0517-super-washing-machines/solution.py
@@ -0,0 +1,38 @@
+# ──────────────────────────────────────────────────
+# Problem  : 517. Super Washing Machines
+# Difficulty: Hard
+# Tags     : Array, Greedy
+# Link     : https://leetcode.com/problems/super-washing-machines/
+# Runtime  : 4 ms (beats 31%)
+# Memory   : 20220000 (beats 13%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def findMinMoves(self, machines: List[int]) -> int:
+        N = len(machines)
+        total_sum = sum(machines)
+        avg = total_sum // N
+
+        if total_sum % N != 0:
+            return -1
+
+        imbalances = [machines[i] - avg for i in range(N)]
+
+        res = float('-inf')
+        D_i = 0
+        for i in range(N):
+            # account for the current boundary
+            res = max(res, abs(D_i))
+
+            # account for the current imbalance
+            res = max(res, imbalances[i])
+
+            # get the next boundary
+            D_i += imbalances[i]
+        
+        return res
+                        
+
