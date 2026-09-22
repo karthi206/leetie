7b5034118ffb61d19ee3ba4de774091e0cb95c32
karthi206 <karthi206@users.noreply.github.com>
2026-09-22 03:27:55 +0000

    leetie: Add 621. Task Scheduler [Medium] (python3)

---
+++ solutions/Medium/0621-task-scheduler/solution.py
@@ -0,0 +1,19 @@
+# ──────────────────────────────────────────────────
+# Problem  : 621. Task Scheduler
+# Difficulty: Medium
+# Tags     : Array, Hash Table, Greedy, Sorting, Heap (Priority Queue), Counting
+# Link     : https://leetcode.com/problems/task-scheduler/
+# Runtime  : 12 ms (beats 95%)
+# Memory   : 20544000 (beats 97%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+from collections import Counter
+class Solution:
+    def leastInterval(self, tasks, n):
+        freq = list(Counter(tasks).values())
+        maxF = max(freq)
+        cnt = freq.count(maxF)
+        return max(len(tasks), (maxF - 1) * (n + 1) + cnt)
