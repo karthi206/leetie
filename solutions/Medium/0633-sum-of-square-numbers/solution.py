56b941b600fc8c951dad36d4a3c16c2ee5a2ef53
karthi206 <karthi206@users.noreply.github.com>
2026-09-23 04:03:14 +0000

    leetie: Add 633. Sum of Square Numbers [Medium] (python3)

--- solutions/Medium/0633-sum-of-square-numbers/solution.py
+++ solutions/Medium/0633-sum-of-square-numbers/solution.py
@@ -3,19 +3,18 @@
 # Difficulty: Medium
 # Tags     : Math, Two Pointers, Binary Search
 # Link     : https://leetcode.com/problems/sum-of-square-numbers/
-# Runtime  : 74 ms (beats 37%)
-# Memory   : 19452000 (beats 28%)
+# Runtime  : 69 ms (beats 40%)
+# Memory   : 19208000 (beats 88%)
 # Language : python3
 # Copyright: (c) 2026 karthi206. All rights reserved.
 # Synced by: leetie
 # ──────────────────────────────────────────────────
 
 from math import sqrt
-
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        for a in range(int(sqrt(c)) + 1):  # Iterate through all possible values of `a`
-            b = sqrt(c - a * a)  # Compute `b` as the square root of `c - a^2`
-            if b == int(b):  # Check if `b` is an integer
-                return True  # If `b` is an integer, return true
-        return False  # If no such pair `(a, b)` is found, return false
+        for a in range(int(sqrt(c)) + 1):  
+            b = sqrt(c - a * a)  
+            if b == int(b):  
+                return True  
+        return False  
