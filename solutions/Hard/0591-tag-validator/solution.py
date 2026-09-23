2ef69cf33bf1452a31fa0572a1b2f801d14d2543
karthi206 <karthi206@users.noreply.github.com>
2026-09-23 03:56:05 +0000

    leetie: Add 591. Tag Validator [Hard] (python3)

---
+++ solutions/Hard/0591-tag-validator/solution.py
@@ -0,0 +1,38 @@
+# ──────────────────────────────────────────────────
+# Problem  : 591. Tag Validator
+# Difficulty: Hard
+# Tags     : String, Stack
+# Link     : https://leetcode.com/problems/tag-validator/
+# Runtime  : 1 ms (beats 29%)
+# Memory   : 19384000 (beats 53%)
+# Language : python3
+# Copyright: (c) 2026 karthi206. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def isValid(self, code: str) -> bool:
+        stack = []
+        i = 0
+        while i < len(code):
+            if i > 0 and not stack:
+                return False
+            if code[i:i+9] == "<![CDATA[":
+                i = code.find("]]>", i + 9)
+                if i == -1:
+                    return False
+                i = i + 3
+            elif code[i: i + 2] == "</" and stack:
+                endTag = stack.pop() + ">"
+                if code[i + 2: i + 2 + len(endTag)] != endTag:
+                    return False
+                i = i + len(endTag) + 2
+            elif code[i] == "<":
+                x = code.find(">", i + 2, i + 11)
+                if x == -1 or not (code[i + 1: x].isalpha() and code[i + 1: x].isupper()):
+                    return False
+                stack.append(code[i + 1: x])
+                i = x + 1
+            else:
+                i += 1
+        return not stack
